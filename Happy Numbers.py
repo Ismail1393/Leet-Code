6f6a4bb4c3b5682eed4ab13290d488e3d93d45d9
ismail1393 <ismailmustaali@gmail.com>
2022-10-12 18:31:52 -0400

    commit

---
+++ Happy Numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self,n : int ) -> bool:
+        visit = set()
+        
+        while n not in visit:
+            visit.add(n)
+            n = self.squareNumbers(n)
+            if n == 1:
+                return True
+        return False
+    
+    def squareNumbers(self, n: int) -> int: 
+        output = 0
+        while n != 0:
+            remainder = n % 10 
+            remainder = remainder **2 
+            output += remainder
+            n=n//10
+        return output 
+    
+            
